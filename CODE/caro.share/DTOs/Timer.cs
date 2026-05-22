492bcd5006e66ff141a5c011dee97385d8ca4f16
Phuc <thienphuc12a1ltt@gmail.com>
2026-05-22 13:54:21 +0700

    update DTOs/Timer.cs

---
+++ CODE/caro.share/DTOs/Timer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caro.share.DTOs
+{
+    public class GameStartNotify
+    {
+        public int roomid { get; set; }
+        public string name_player1 { get; set; }
+        public string name_player2 { get; set; }
+        public int timeSeconds { get; set; }
+    }
+}
