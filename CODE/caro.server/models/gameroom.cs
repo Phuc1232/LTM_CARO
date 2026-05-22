26f6de889988617f4ed51785ae3ab1cab916dd87
Phuc <thienphuc12a1ltt@gmail.com>
2026-05-22 13:33:09 +0700

    update gameroom

--- CODE/caro.server/models/gameroom.cs
+++ CODE/caro.server/models/gameroom.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using caro.server.network;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace caro.server.models
 {
-    internal class gameroom
+    public class gameroom
     {
+        public int RoomID { get; set; }
+        public ClientHandle player1 { get; set; }
+        public ClientHandle player2 { get; set; }
+        public int TimeSecondPerPlayer { get; set; } //thoi gian goc cua moi player
+        public int RemainingTimeP1 { get; set; }
+        public int RemainingTimeP2 { get; set; }
+        public string CurrentTurn { get; set; }
+        public CancellationTokenSource cts { get; set; } // quan ly thoi gian ket thuc
+        public bool IsGameActive { get; set; }
     }
 }
