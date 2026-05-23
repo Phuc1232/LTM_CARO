ceab25ae701b5c3167ddf5712c52b1b815db438b
Phuc <thienphuc12a1ltt@gmail.com>
2026-05-23 11:03:23 +0700

    update  TiimerExprise + TimeUpdate

--- CODE/caro.share/DTOs/Timer.cs
+++ CODE/caro.share/DTOs/Timer.cs
@@ -4,11 +4,23 @@ using System.Text;
 
 namespace caro.share.DTOs
 {
-    public class GameStartNotify
+    public class GameStartNotifyDTO
     {
         public string roomid { get; set; }
         public string name_player1 { get; set; }
         public string name_player2 { get; set; }
         public int timeSeconds { get; set; }
+    }
+    public class TimerUpdateDTO
+    {
+        public int RemainingTimePlayer1 { get; set; }
+        public int RemainingTimePlayer2 { get; set; }
+        public string CurrentTurnUseName { get; set; }
+    }
+    public class TimerExpiredDTO
+    {
+        public string loser { get; set; }
+        public string winner { get; set; }
+        public string message { get; set; }
     }
 }
