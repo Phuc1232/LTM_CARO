4dcf43db84cb3338bb270217e6991254101ce558
Phuc <thienphuc12a1ltt@gmail.com>
2026-05-29 15:02:03 +0700

    Update Func InitializeDatabaseAsync

---
+++ CODE/caro.server/services/DatabaseServices.cs
@@ -0,0 +1,41 @@
+﻿using caro.server.database;
+using caro.server.form;
+using caro.server.network;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Text;
+
+namespace caro.server.services
+{
+    public class DatabaseServices
+    {
+        private static readonly Lazy<DatabaseServices> _instance = new Lazy<DatabaseServices>(() => new DatabaseServices());
+
+        public static DatabaseServices Instance => _instance.Value;
+
+        // Khởi tạo phiên làm việc với Database
+        public async Task<bool> InitializeDatabaseAsync()
+        {
+            try
+            {
+                using (var context = new CaroDbContext())
+                {
+                    var CanConnect = await context.Database.CanConnectAsync();
+                    if (!CanConnect)
+                    {
+                        TCPServerManager.Log("Kết Nối Database Thất Bại!!!!");
+                    }
+                    TCPServerManager.Log("Kết Nối Database Thành Công!!!");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Database Error] Khởi tạo thất bại!!: {ex.Message}");
+                return false;
+            }
+        }
+
+    }
+}
