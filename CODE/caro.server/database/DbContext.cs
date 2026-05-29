7db82aa3c52fc84a6adab0b38cc64c3a4c2b40fa
dangnguyen0512 <dannguyen123222132@gmail.com>
2026-05-29 13:42:08 +0700

    Update DbContext

---
+++ CODE/caro.server/database/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace caro.server.database
+{
+    public class CaroDbContext : DbContext
+    {
+        public DbSet<MatchHistoryEntity> MatchHistories { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(
+                    "Host=localhost;Port=5432;Database=caro_db;Username=postgres;Password=1234"
+                );
+            }
+        }
+    }
+}
