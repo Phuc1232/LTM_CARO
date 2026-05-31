ea29ec1ae2821f4fe3724d68557ea0d4c0e32db6
yuutoryoki2-maker <yuutoryoki2@gmail.com>
2026-05-31 21:40:08 +0700

    Update UI - Giao Dien Home.cs

---
+++ CODE/caro.client/ui_components/MenuButton.Designer.cs
@@ -0,0 +1,95 @@
+﻿using System.Net.NetworkInformation;
+
+namespace caro.client.ui_components
+{
+    partial class MenuButton
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        private void InitializeComponet()
+        {
+            btnMenu = new Button();
+            SuspendLayout();
+
+            btnMenu.BackColor = Color.FromArgb(255, 128, 255);
+            btnMenu.BackgroundImageLayout = ImageLayout.Center;
+            btnMenu.Dock = DockStyle.Fill;
+            btnMenu.FlatAppearance.BorderSize = 0;
+            btnMenu.FlatStyle = FlatStyle.Flat;
+            btnMenu.Font = new Font("Seogoe UI", 14F);
+            btnMenu.Location = new Point(0, 0);
+            btnMenu.Name = "btnMenu";
+            btnMenu.Size = new Size(250, 60);
+            btnMenu.TabIndex = 0;
+            btnMenu.Text = "Menu";
+            btnMenu.UseVisualStyleBackColor = false;
+            btnMenu.Click += BtnMenu_Click;
+
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnMenu);
+            Name = "MenuButton";
+            Size = new Size(250, 60);
+            ResumeLayout(false);
+        }
+
+        private void BtnMenu_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMenu = new Button();
+            SuspendLayout();
+            // 
+            // btnMenu
+            // 
+            btnMenu.BackColor = Color.FromArgb(255, 192, 192);
+            btnMenu.Dock = DockStyle.Fill;
+            btnMenu.FlatAppearance.BorderSize = 0;
+            btnMenu.FlatStyle = FlatStyle.Flat;
+            btnMenu.Font = new Font("Segoe UI", 14F);
+            btnMenu.Location = new Point(0, 0);
+            btnMenu.Name = "btnMenu";
+            btnMenu.Size = new Size(250, 60);
+            btnMenu.TabIndex = 0;
+            btnMenu.Text = "Menu";
+            btnMenu.UseVisualStyleBackColor = false;
+            // 
+            // MenuButton
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnMenu);
+            Name = "MenuButton";
+            Size = new Size(250, 60);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMenu;
+    }
+}
---
+++ CODE/caro.client/ui_components/MenuButton.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace caro.client.ui_components
+{
+    public partial class MenuButton : UserControl
+    {
+        [Browsable(true)]
+        [Category("Appearance")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public Color HoverBackColor { get; set; } = Color.DeepSkyBlue;
+
+        [Browsable(true)]
+        [Category("Appearance")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public Color HoverForeColor { get; set; } = Color.White;
+
+        public MenuButton()
+        {
+            InitializeComponent();
+
+            // subscribe safely only if designer created the inner button
+            if (btnMenu != null)
+            {
+                btnMenu.MouseEnter += BtnMenu_MouseEnter;
+                btnMenu.MouseLeave += BtnMenu_MouseLeave;
+
+                // Ensure initial sync between base.Text and the inner button
+                if (!string.IsNullOrEmpty(base.Text))
+                {
+                    btnMenu.Text = base.Text;
+                }
+            }
+        }
+
+        private void BtnMenu_MouseEnter(object? sender, EventArgs e)
+        {
+            if (btnMenu != null)
+            {
+                btnMenu.BackColor = HoverBackColor;
+                btnMenu.ForeColor = HoverForeColor;
+            }
+        }
+
+        private void BtnMenu_MouseLeave(object? sender, EventArgs e)
+        {
+            if (btnMenu != null)
+            {
+                btnMenu.BackColor = Color.DodgerBlue;
+                btnMenu.ForeColor = Color.White;
+            }
+        }
+
+        [Browsable(true)]
+        [Category("Appearance")]
+        [Description("The text associated with the control.")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [Bindable(true)]
+        [Localizable(true)]
+        public override string Text
+        {
+            get => btnMenu?.Text ?? base.Text;
+            set
+            {
+                base.Text = value;
+
+                if (btnMenu != null)
+                {
+                    btnMenu.Text = value;
+                    Invalidate();
+                }
+
+                OnTextChanged(EventArgs.Empty);
+            }
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
