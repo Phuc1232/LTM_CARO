480153efd563cafdffa7e959b0a7e8f176e195ff
Phuc <thienphuc12a1ltt@gmail.com>
2026-06-03 14:04:31 +0700

    Update History

--- CODE/caro.share/DTOs/History.cs
+++ CODE/caro.share/DTOs/History.cs
@@ -14,10 +14,9 @@ namespace caro.share.DTOs
         public string MatchType { get; set; } = null!; // "PvP" hoặc "AI"
         public DateTime PlayedAt { get; set; }
         public string MovesData { get; set; } = string.Empty; // Chuỗi tọa độ nước đi: "phuc:7,7;an:8,8"
-
-        public class MatchHistoryResponeDTO
-        {
-            public List<MatchHistoryItemDTO> histories { get; set; } = new List<MatchHistoryItemDTO>(); 
-        }
+    }
+    public class MatchHistoryResponseDTO
+    {
+        public List<MatchHistoryItemDTO> histories { get; set; } = new List<MatchHistoryItemDTO>();
     }
 }
