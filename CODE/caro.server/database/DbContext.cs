de139e2a5eabced2664d6de22a169772409a9c51
Phuc <thienphuc12a1ltt@gmail.com>
2026-06-04 21:02:05 +0700

    Update

--- CODE/caro.server/database/DbContext.cs
+++ CODE/caro.server/database/DbContext.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using DotNetEnv;
 namespace caro.server.database
 {
     public class CaroDbContext : DbContext
     {
+        
         public DbSet<MatchHistoryEntity> MatchHistories { get; set; } = null!;
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
+                DotNetEnv.Env.TraversePath().Load();
+                string? host = Environment.GetEnvironmentVariable("DB_SERVER");
+                string? database_name = Environment.GetEnvironmentVariable("DB_NAME");
+                string? user = Environment.GetEnvironmentVariable("DB_USER");
+                string? password = Environment.GetEnvironmentVariable("DB_PASSWORD");
                 optionsBuilder.UseNpgsql(
-                    "Host=localhost;Port=5432;Database=caro_db;Username=postgres;Password=1234"
+                    $"Host={host};Port=5432;Database={database_name};Username={user};Password={password}"
                 );
             }
         }
