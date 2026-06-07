8436c5bb423fd3aaa278df6fef68287d8ddfbbe5
Phuc <thienphuc12a1ltt@gmail.com>
2026-06-08 01:00:04 +0700

    Update Func GetValidMove

--- CODE/caro.ai/AIServices.cs
+++ CODE/caro.ai/AIServices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 
@@ -38,6 +39,39 @@ namespace caro.ai
             return r >= 0 && r < SIZE && c >= 0 && c < SIZE;
         }
 
+        public static List<(int r, int c)> GetVaildMove(int[,] boardstate)
+        {
+            var moves = new HashSet<(int r, int c)>();
+            bool hasPieces = false;
 
+            for (int r = 0; r < SIZE; r++)
+            {
+                for (int c = 0; c < SIZE; c++)
+                {
+                    if (boardstate[r, c] != EMPTY)
+                    {
+                        hasPieces = true;
+                        for (int dr = -2; dr <= 2; dr++)
+                        {
+                            for (int dc = -2; dc <= 2; dc++)
+                            {
+                                int nr = r + dr;
+                                int nc = c + dc;
+                                if (boardstate[nr, nc] == EMPTY)
+                                {
+                                    moves.Add((nr, nc));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!hasPieces)
+            {
+                return new List<(int r, int c)> { (CENTER, CENTER) };
+
+            }
+            return moves.ToList();
+        }
     }
 }
