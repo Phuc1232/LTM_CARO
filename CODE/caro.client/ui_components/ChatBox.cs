ba992e9de16276bc1ff60bd472d5b06303bd0209
WeiZhangUwU <htan.phuyen@gmail.com>
2026-06-09 16:55:37 +0700

    Update chat

---
+++ CODE/caro.client/ui_components/ChatBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caro.client.ui_components
+{
+    public partial class ChatBox : UserControl
+    {
+        public ChatBox()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ChatBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
