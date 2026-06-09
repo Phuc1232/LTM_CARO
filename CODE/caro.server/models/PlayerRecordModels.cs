7a1765ba3a6d42ec5cf8a68488387cacaeecc2ef
dangnguyen0512 <dannguyen123222132@gmail.com>
2026-06-09 19:11:09 +0700

    Add Model PlayerRecord

---
+++ CODE/caro.server/models/PlayerRecordModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace caro.server.models
+{
+    public class PlayerRecordModels
+    {
+        public string Username { get; set; } = null!;
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Draws { get; set; }
+        public int WinStreak { get; set; }
+        public int MaxWinStreak { get; set; }
+        public int ShortestWinMoves { get; set; }
+    }
+}
