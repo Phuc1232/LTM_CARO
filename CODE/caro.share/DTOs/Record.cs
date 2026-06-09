7936c6fb9f97437f3f98ab3f647d059856ea489b
dangnguyen0512 <dannguyen123222132@gmail.com>
2026-06-09 19:18:23 +0700

    Add RecordDTO

---
+++ CODE/caro.share/DTOs/Record.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caro.share.DTOs
+{
+    public class BestRecordItemDTO
+    {
+        public string Username { get; set; } = null!;
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Draws { get; set; }
+        public int MaxWinStreak { get; set; }
+        public int ShortestWinMoves { get; set; } // int.MaxValue nếu chưa có trận thắng nào
+    }
+
+   
+}
