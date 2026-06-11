03bb9a629bf803751b7372065d369d1f2aee1c96
WeiZhangUwU <htan.phuyen@gmail.com>
2026-06-11 14:04:44 +0700

    Update Program

--- CODE/caro.client/Program.cs
+++ CODE/caro.client/Program.cs
@@ -13,7 +13,7 @@ namespace caro.client
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new GameBoard());
+            Application.Run(new Login());
         }
     }
 }
