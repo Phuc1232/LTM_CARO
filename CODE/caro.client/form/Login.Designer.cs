34b942152235bcffef8d794ec5c877b55a38c10d
WeiZhangUwU <htan.phuyen@gmail.com>
2026-06-11 14:03:46 +0700

    Update Login

---
+++ CODE/caro.client/form/Login.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace caro.client.form
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Label2 = new Label();
+            Label3 = new Label();
+            txtUsername = new TextBox();
+            txtIpAddress = new TextBox();
+            btnLogin = new caro.client.ui_components.MenuButton();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Segoe UI", 24F);
+            label1.Location = new Point(642, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 45);
+            label1.TabIndex = 0;
+            label1.Text = "CARO";
+            // 
+            // Label2
+            // 
+            Label2.AutoSize = true;
+            Label2.BackColor = Color.White;
+            Label2.Font = new Font("Segoe UI", 16F);
+            Label2.Location = new Point(563, 149);
+            Label2.Name = "Label2";
+            Label2.Size = new Size(111, 30);
+            Label2.TabIndex = 1;
+            Label2.Text = "Username";
+            // 
+            // Label3
+            // 
+            Label3.AutoSize = true;
+            Label3.BackColor = Color.White;
+            Label3.Font = new Font("Segoe UI", 16F);
+            Label3.Location = new Point(545, 202);
+            Label3.Name = "Label3";
+            Label3.Size = new Size(129, 30);
+            Label3.TabIndex = 2;
+            Label3.Text = "IP ADDRESS";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(695, 156);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(182, 23);
+            txtUsername.TabIndex = 3;
+            // 
+            // txtIpAddress
+            // 
+            txtIpAddress.Location = new Point(695, 211);
+            txtIpAddress.Name = "txtIpAddress";
+            txtIpAddress.Size = new Size(182, 23);
+            txtIpAddress.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.White;
+            btnLogin.ForeColor = Color.White;
+            btnLogin.HoverBackColor = Color.DeepSkyBlue;
+            btnLogin.HoverForeColor = Color.White;
+            btnLogin.Location = new Point(594, 264);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(206, 37);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "LOGIN";
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 30, 46);
+            ClientSize = new Size(1402, 690);
+            Controls.Add(btnLogin);
+            Controls.Add(txtIpAddress);
+            Controls.Add(txtUsername);
+            Controls.Add(Label3);
+            Controls.Add(Label2);
+            Controls.Add(label1);
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label Label2;
+        private Label Label3;
+        private TextBox txtUsername;
+        private TextBox txtIpAddress;
+        private ui_components.MenuButton btnLogin;
+    }
+}
---
+++ CODE/caro.client/form/Login.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using caro.client.network;
+using caro.share.DTOs;
+using caro.share.DTOs.Constants;
+
+namespace caro.client.form
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            TCPClientManager.Instance.OnLoginResponse += HandleLoginResponse;
+        }
+        private void HandleLoginResponse(LoginResponseDTO response)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(() => HandleLoginResponse(response));
+                return;
+            }
+
+            if (response.isSuccess)
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+
+                Home home = new Home();
+                home.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show(response.message);
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string ip = txtIpAddress.Text.Trim();
+
+            if (username == "" || ip == "")
+            {
+                MessageBox.Show("Vui lòng nhập Username và IP Address!");
+                return;
+            }
+            bool connected = await TCPClientManager.Instance.ConnectAsync(ip, 8888);
+
+            if (connected)
+            {
+                var loginRequest = new LoginRequestDTO
+                {
+                    username = username
+                };
+
+                await TCPClientManager.Instance.SendPacketAsync(
+                    PacketType.LoginRequest,
+                    loginRequest
+                );
+
+                MessageBox.Show("Đã gửi yêu cầu đăng nhập lên server!");
+
+       
+            }
+            else
+            {
+                MessageBox.Show(
+           "Không thể kết nối server. Hãy kiểm tra IP hoặc mở server trước!",
+           "Connection Error",
+           MessageBoxButtons.OK,
+           MessageBoxIcon.Error);
+            }
+        }
+    }
+}
