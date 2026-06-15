5bca78ff1051c3541ab7e1d3d7f6ba8721edfd31
WeiZhangUwU <htan.phuyen@gmail.com>
2026-06-15 19:43:21 +0700

    Update Matching

---
+++ CODE/caro.client/form/MatchMaking.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace caro.client.form
+{
+    partial class MatchMaking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstOnlinePlayers = new ListBox();
+            btnChallenge = new caro.client.ui_components.MenuButton();
+            btnBack = new caro.client.ui_components.MenuButton();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(30, 30, 46);
+            label1.Font = new Font("Segoe UI", 36F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(518, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(383, 65);
+            label1.TabIndex = 6;
+            label1.Text = "MATCH MAKING";
+            // 
+            // lstOnlinePlayers
+            // 
+            lstOnlinePlayers.BackColor = Color.FromArgb(42, 42, 60);
+            lstOnlinePlayers.ForeColor = Color.White;
+            lstOnlinePlayers.FormattingEnabled = true;
+            lstOnlinePlayers.Location = new Point(559, 108);
+            lstOnlinePlayers.Name = "lstOnlinePlayers";
+            lstOnlinePlayers.Size = new Size(300, 379);
+            lstOnlinePlayers.TabIndex = 7;
+            // 
+            // btnChallenge
+            // 
+            btnChallenge.HoverBackColor = Color.DeepSkyBlue;
+            btnChallenge.HoverForeColor = Color.White;
+            btnChallenge.Location = new Point(587, 521);
+            btnChallenge.Name = "btnChallenge";
+            btnChallenge.Size = new Size(250, 60);
+            btnChallenge.TabIndex = 8;
+            btnChallenge.Text = "Thách đấu";
+            btnChallenge.Click += btnChallenge_Click_1;
+            // 
+            // btnBack
+            // 
+            btnBack.HoverBackColor = Color.DeepSkyBlue;
+            btnBack.HoverForeColor = Color.White;
+            btnBack.Location = new Point(587, 601);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(250, 60);
+            btnBack.TabIndex = 9;
+            btnBack.Text = "Quay lại";
+            // 
+            // MatchMaking
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 30, 46);
+            ClientSize = new Size(1402, 690);
+            Controls.Add(btnBack);
+            Controls.Add(btnChallenge);
+            Controls.Add(lstOnlinePlayers);
+            Controls.Add(label1);
+            Name = "MatchMaking";
+            Text = "MatchMaking";
+            Load += MatchMaking_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstOnlinePlayers;
+        private ui_components.MenuButton btnChallenge;
+        private ui_components.MenuButton btnBack;
+    }
+}
---
+++ CODE/caro.client/form/MatchMaking.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using caro.client.network;
+using caro.share.DTOs;
+using caro.share.DTOs.Constants;
+
+namespace caro.client.form
+{
+    public partial class MatchMaking : Form
+    {
+        public MatchMaking()
+        {
+            InitializeComponent();
+            TCPClientManager.Instance.OnOnlinePlayerListUpdated += UpdateOnlinePlayers;
+            TCPClientManager.Instance.OnChallengeReceived += ReceiveChallenge;
+            TCPClientManager.Instance.OnChallengeResult += ChallengeResult;
+            TCPClientManager.Instance.OnGameStarted += GameStarted;
+            if (TCPClientManager.Instance.LastOnlinePlayers != null)
+            {
+                UpdateOnlinePlayers(TCPClientManager.Instance.LastOnlinePlayers);
+            }
+        }
+        private void UpdateOnlinePlayers(OnlinePlayerListDTO dto)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(() => UpdateOnlinePlayers(dto));
+                return;
+            }
+
+            lstOnlinePlayers.Items.Clear();
+
+            foreach (string player in dto.players)
+            {
+                // Không hiện chính mình
+                if (player == TCPClientManager.Instance.CurrentUsername)
+                    continue;
+
+                lstOnlinePlayers.Items.Add(player);
+            }
+        }
+
+        private async void btnChallenge_Click(object sender, EventArgs e)
+        {
+            if (lstOnlinePlayers.SelectedItem == null)
+            {
+                MessageBox.Show("Hãy chọn người chơi để thách đấu!");
+                return;
+            }
+
+            string targetUsername = lstOnlinePlayers.SelectedItem.ToString();
+
+            await TCPClientManager.Instance.SendPacketAsync(
+                PacketType.ChallengeRequest,
+                new ChallengeRequestDTO
+                {
+                    targetUsername = targetUsername
+                });
+
+            MessageBox.Show($"Đã gửi lời thách đấu tới {targetUsername}");
+        }
+
+        private async void ReceiveChallenge(ChallengeNotifyDTO dto)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(() => ReceiveChallenge(dto));
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                $"{dto.fromUsername} muốn thách đấu bạn. Đồng ý không?",
+                "Lời mời thách đấu",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            await TCPClientManager.Instance.SendPacketAsync(
+                PacketType.ChallengeResponse,
+                new ChallengeResponseDTO
+                {
+                    roomId = dto.roomId,
+                    isAccepted = result == DialogResult.Yes
+                });
+        }
+
+        private void ChallengeResult(ChallengeResultDTO dto)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(() => ChallengeResult(dto));
+                return;
+            }
+
+            MessageBox.Show(dto.message);
+
+            if (dto.isAccepted)
+            {
+                GameBoard gameBoard = new GameBoard();
+                gameBoard.Show();
+                this.Hide();
+            }
+        }
+        private void GameStarted(GameStartNotifyDTO dto)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(() => GameStarted(dto));
+                return;
+            }
+
+            GameBoard gameBoard = new GameBoard();
+            gameBoard.Show();
+
+            this.Hide();
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MatchMaking_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnChallenge_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Đã bấm nút thách đấu");
+
+            if (lstOnlinePlayers.SelectedItem == null)
+            {
+                MessageBox.Show("Hãy chọn người chơi để thách đấu!");
+                return;
+            }
+
+            string targetUsername = lstOnlinePlayers.SelectedItem.ToString();
+
+            await TCPClientManager.Instance.SendPacketAsync(
+                PacketType.ChallengeRequest,
+                new ChallengeRequestDTO
+                {
+                    targetUsername = targetUsername
+                });
+
+            MessageBox.Show($"Đã gửi lời thách đấu tới {targetUsername}");
+        }
+    }
+}
